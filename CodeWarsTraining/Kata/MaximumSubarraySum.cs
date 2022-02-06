e4db1e8a6803a6b900698665575f1c1d424ac6ac
Lukasz <ubalcerak@gmail.com>
2022-02-06 19:32:06 +0100

    Task Three done

---
+++ CodeWarsTraining/Kata/MaximumSubarraySum.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTraining.Kata
+{
+    public class MaximumSubarraySum
+    {
+        public static int Sum(int[] arr)
+        {
+            var sum = 0;
+
+            for(int i = 0; i <arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    var sumDown = 0;
+                    var sumUp = 0;
+                    var tempSumDown = 0;
+                    var tempSumUp = 0;
+
+                    for (int j = i; j == 0; j--)
+                    {
+                        tempSumDown += arr[j];
+                        sumDown = sumDown > tempSumDown ? sumDown : tempSumDown;
+                    }
+
+                    for (int k = i; k < arr.Length; k++)
+                    {
+                        tempSumUp += arr[k];
+                        sumUp = sumUp > tempSumUp ? sumUp : tempSumUp;
+                    }
+
+                    if(sumUp > sum)
+                    {
+                        sum = sumUp;
+                    } else if (sumDown > sum)
+                    {
+                        sum = sumDown;
+                    } else
+                    {
+                        continue;
+                    }
+
+                } else
+                {
+                    continue;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
